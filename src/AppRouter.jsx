ce71bcede75edc7968e3ee8f9be6488200da3e01
khadar-oothuru <khadaroothuru@gmail.com>
2024-11-12 04:29:02 +0530

    adding app roouter

---
+++ src/AppRouter.jsx
@@ -0,0 +1,20 @@
+// src/AppRouter.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TotalWaste from "./pages/TotalWaste";
+import RealTimeData from "./pages/RealTime";
+import Demo from "./pages/demo";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/total-waste" element={<TotalWaste />} />
+      <Route path="/demo" element={<Demo />} />
+      <Route path="/real-time-data" element={<RealTimeData />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
